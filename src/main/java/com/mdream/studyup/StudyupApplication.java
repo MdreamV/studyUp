d11536f94d3bdfd2ff9934ce1f4bc9e2c5443610
sunmengnan <s673789825@163.com>
2022-04-09 16:00:48 +0800

    feature(0001): 新建项目

---
+++ src/main/java/com/mdream/studyup/StudyupApplication.java
@@ -0,0 +1,13 @@
+package com.mdream.studyup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdream/studyup/StudyupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdream.studyup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
